27da19cac99a5dd02cf4c848d33750482eab1f59
Prateek Shrivastava <prateekplasmaz9801@gmail.com>
2026-07-09 13:52:47 +0530

    Initial Python practice programs

---
+++ 01_Basics/swap_in_2variables.py
@@ -0,0 +1,16 @@
+x=100
+y=20
+print("Before swap")
+print(x)
+print(y)
+x=x+y
+y=x-y
+x=x-y
+print("After swap")
+print(x)
+print(y)
+a,b,c=100,"Friendly",10.5 #use of comma operator
+a,c=c,a
+print(a,c)
+b=b+" Ranger"
+print(b)
