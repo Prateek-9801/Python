8ef21e346d52db9258ed28837f3167f3debe62b2
Prateek Shrivastava <prateekplasmaz9801@gmail.com>
2026-07-09 14:32:20 +0530

    Enhance comments in swap_in_2variables.py
    Added comments to explain the swapping logic and variable usage.

--- 01_Basics/swap_in_2variables.py
+++ 01_Basics/swap_in_2variables.py
@@ -1,3 +1,4 @@
+#In this code I learnt how to swap two values stored in two different variables one by mathematically and other by simple python syntax
 x=100
 y=20
 print("Before swap")
@@ -12,5 +13,5 @@ print(y)
 a,b,c=100,"Friendly",10.5 #use of comma operator
 a,c=c,a
 print(a,c)
-b=b+" Ranger"
+b=b+" Ranger" # here + means to append the value written after it
 print(b)
