8954a2b4cba0a9e4b486b2cc737cf881334d7a60
Prateek-9801 <prateekplasmaz9801@gmail.com>
2026-07-11 23:40:23 +0530

    Add sets program

---
+++ 02_Variables_and_Data_types/sets.py
@@ -0,0 +1,17 @@
+#Set in python
+#Distinct elements
+#unordered
+#no indexing
+#unio, itersection, set difference, etc are fast
+#uses hashing internally.
+#1st program
+s1={10,20,30}
+print(s1)
+s2=set([20,30,40])
+print(s2)
+s3={}
+print(type(s3))
+s4=set()
+print(type(s4))
+print(s4)
+
---
+++ 02_Variables_and_Data_types/sets_02.py
@@ -0,0 +1,12 @@
+#Program to demostrate removal of item
+s={10,30,20,40}
+s.discard(30)
+print(s)
+s.remove(20)
+print(s)
+s.clear()
+print(s)
+s.add(50)
+print(s)
+del s
+
