8467f4e9c3f4989f152024d9d2cd5dfff694e7bb
Prateek-9801 <prateekplasmaz9801@gmail.com>
2026-07-13 23:33:30 +0530

    Add operations in two sets

--- 02_Variables_and_Data_types/sets_03.py
+++ 02_Variables_and_Data_types/sets_03.py
@@ -1,3 +1,4 @@
+#operations on two sets
 s1={2,4,6,8}
 s2={3,6,9}
 print(s1|s2) #union
@@ -8,3 +9,11 @@ print(s1-s2) #{8,2,4} which are present in s1 but not in s2
 #s1.difference(s2)
 print(s1^s2) #{2,3,4,8,9}symmetric difference, not common elements
 #s1.symmetric_difference(s2)
+#more operations on two sets
+s3={2,4,6,8}
+s4={4,8}
+print(s3.isdisjoint(s4)) # if no common element = True
+print(s3<=s4) # s3.issubset(s4) s3 is subset of s4 or not
+print(s3<s4) # proper subset 
+print(s3>=s4) # s3.issuperset(s4) 
+print(s3>s4) # proper superset
---
+++ 02_Variables_and_Data_types/sets_04.py
@@ -0,0 +1,6 @@
+#other operations
+s={10,30,20,40} 
+print(len(s)) # 4
+print(20 in s) # True  #in operator is fast on sets compared to lists beacause sets use hashing internally
+print(50 in s) # False
+
