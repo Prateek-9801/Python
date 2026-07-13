f56cbb8bfb29a6d55fea7818f825a767dbaac180
Prateek-9801 <prateekplasmaz9801@gmail.com>
2026-07-13 22:55:02 +0530

    Add comments

--- 02_Variables_and_Data_types/sets_03.py
+++ 02_Variables_and_Data_types/sets_03.py
@@ -1,6 +1,10 @@
 s1={2,4,6,8}
 s2={3,6,9}
-print(s1|s2)
-print(s1 & s2)
-print(s1-s2)
-print(s1^s2)
+print(s1|s2) #union
+#s1.union(s2) alternate way
+print(s1 & s2) #intersection
+#s1.intersection(s2)
+print(s1-s2) #{8,2,4} which are present in s1 but not in s2
+#s1.difference(s2)
+print(s1^s2) #{2,3,4,8,9}symmetric difference, not common elements
+#s1.symmetric_difference(s2)
