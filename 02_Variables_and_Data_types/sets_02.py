01a4b49e4e9a60b5aa691163e4d2526c351484b3
Prateek-9801 <prateekplasmaz9801@gmail.com>
2026-07-13 23:04:16 +0530

    Add comment

--- 02_Variables_and_Data_types/sets_02.py
+++ 02_Variables_and_Data_types/sets_02.py
@@ -1,12 +1,12 @@
 #Program to demostrate removal of item
 s={10,30,20,40}
-s.discard(30)
+s.discard(30) #if the item is present in the set it is removed but if the item is not present it not going to do anything
 print(s)
-s.remove(20)
+s.remove(20) #if the item is present in the set it is removed else raise an error that's the difference between discard and remove
 print(s)
-s.clear()
+s.clear() # set() #everything will be removed from the set and will give an empty set
 print(s)
 s.add(50)
 print(s)
-del s
+del s #the whole object is revomed which means empty set will also be removed
 
